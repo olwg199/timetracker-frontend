024819c3864f358dc5160967f59800d7e2235938
Oleg Yastrebov <olwg199@gmail.com>
2021-01-09 20:10:43 -0500

    Refactor YearView component

--- src/components/mainArea/YearView.jsx
+++ src/components/mainArea/YearView.jsx
@@ -2,33 +2,43 @@ import React from "react";
 import Month from "../general/Month";
 
 function MonthView() {
-    // const monthNames = ["January", "February", "March", "April"];
     const monthNames = ["January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"];
 
     return (
         <div className="container">
-            {monthNames.reduce(function (acc, month, index, arr) {
-
-                acc.Quarter.push(<Month month={month} />);
-
-                if ((index + 1) % 4 === 0 && index !== 0) {
-                    acc.Year.push(
-                        <div className="row">
-                            {acc.Quarter}
-                        </div>
-                    );
-                    acc.Quarter = [];
-                }
-                if ((index + 1) === arr.length) {
-                    return acc.Year;
-                } else {
-                    return acc;
-                }
-            }, { Quarter: [], Year: [] })}
+            {
+                generateYearViewStructure(monthNames, 4)
+            }
         </div>
 
     );
+}
+
+function generateYearViewStructure(months, itemInRow = 4) {
+
+    return months.reduce(function (acc, month, index, arr) {
+        acc.Quarter.push(<Month month={month} />);
+
+        //Logic of devading months into rows views.
+        if ((index + 1) % itemInRow === 0 && index !== 0) {
+            acc.Year.push(
+                <div className="row">
+                    {acc.Quarter}
+                </div>
+            );
+            acc.Quarter = [];
+        }
+
+        //If last item - return generated view for year
+        if ((index + 1) === arr.length) {
+            return acc.Year;
+        } else {
+            return acc;
+        }
+
+    }, { Quarter: [], Year: [] })
+
 }
 
 export default MonthView;
