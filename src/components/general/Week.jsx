49a79bddba9cc7401b4cef9fc32158f9ff05e9e6
Oleg Yastrebov <olwg199@gmail.com>
2021-01-22 21:23:24 -0500

    Add autogeneration for week structure

--- src/components/general/Week.jsx
+++ src/components/general/Week.jsx
@@ -1,177 +1,43 @@
 import React from "react";
 
 function Week(props) {
+    const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
     return (
         <div className="days">
-            <div className="day">
-                <div className="title">
-                    Saturday
-                </div>
-                <ul className="todo-list">
-                    <li className="todo-list-item">
-                        <div className="description">
-                        Lorem ipsum dolor sit amet
-                        </div>
-                        <div className="action-buttons">
-                            <i class="fas fa-trash-alt"></i>
-                        </div>
-                    </li>
-                    <li className="todo-list-item">
-                    <div className="description">
-                        Lorem ipsum dolor sit amet
-                        </div>
-                        <div className="action-buttons">
-                            <i class="fas fa-trash-alt delete"></i>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-
-            <div className="day">
-                <div className="title">
-                    Saturday
-                </div>
-                <ul className="todo-list">
-                    <li className="todo-list-item">
-                        <div className="description">
-                        Lorem ipsum dolor sit amet
-                        </div>
-                        <div className="action-buttons">
-                            <i class="fas fa-trash-alt"></i>
-                        </div>
-                    </li>
-                    <li className="todo-list-item">
-                    <div className="description">
-                        Lorem ipsum dolor sit amet
-                        </div>
-                        <div className="action-buttons">
-                            <i class="fas fa-trash-alt delete"></i>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-
-            <div className="day">
-                <div className="title">
-                    Saturday
-                </div>
-                <ul className="todo-list">
-                    <li className="todo-list-item">
-                        <div className="description">
-                        Lorem ipsum dolor sit amet
-                        </div>
-                        <div className="action-buttons">
-                            <i class="fas fa-trash-alt"></i>
-                        </div>
-                    </li>
-                    <li className="todo-list-item">
-                    <div className="description">
-                        Lorem ipsum dolor sit amet
-                        </div>
-                        <div className="action-buttons">
-                            <i class="fas fa-trash-alt delete"></i>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-
-            <div className="day">
-                <div className="title">
-                    Saturday
-                </div>
-                <ul className="todo-list">
-                    <li className="todo-list-item">
-                        <div className="description">
-                        Lorem ipsum dolor sit amet
-                        </div>
-                        <div className="action-buttons">
-                            <i class="fas fa-trash-alt"></i>
-                        </div>
-                    </li>
-                    <li className="todo-list-item">
-                    <div className="description">
-                        Lorem ipsum dolor sit amet
-                        </div>
-                        <div className="action-buttons">
-                            <i class="fas fa-trash-alt delete"></i>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-
-            <div className="day">
-                <div className="title">
-                    Saturday
-                </div>
-                <ul className="todo-list">
-                    <li className="todo-list-item">
-                        <div className="description">
-                        Lorem ipsum dolor sit amet
-                        </div>
-                        <div className="action-buttons">
-                            <i class="fas fa-trash-alt"></i>
-                        </div>
-                    </li>
-                    <li className="todo-list-item">
-                    <div className="description">
-                        Lorem ipsum dolor sit amet
-                        </div>
-                        <div className="action-buttons">
-                            <i class="fas fa-trash-alt delete"></i>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-
-            <div className="day">
-                <div className="title">
-                    Saturday
-                </div>
-                <ul className="todo-list">
-                    <li className="todo-list-item">
-                        <div className="description">
-                        Lorem ipsum dolor sit amet
-                        </div>
-                        <div className="action-buttons">
-                            <i class="fas fa-trash-alt"></i>
-                        </div>
-                    </li>
-                    <li className="todo-list-item">
-                    <div className="description">
-                        Lorem ipsum dolor sit Lorem ipsum dolor sit 
-                        </div>
-                        <div className="action-buttons">
-                            <i class="fas fa-trash-alt delete"></i>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-
-            <div className="day">
-                <div className="title">
-                    Saturday
-                </div>
-                <ul className="todo-list">
-                    <li className="todo-list-item">
-                        <div className="description">
-                        Lorem ipsum dolor  amet
-                        </div>
-                        <div className="action-buttons">
-                            <i class="fas fa-trash-alt"></i>
-                        </div>
-                    </li>
-                    <li className="todo-list-item">
-                    <div className="description">
-                        Lorem ipsum  sit amet
-                        </div>
-                        <div className="action-buttons">
-                            <i class="fas fa-trash-alt delete"></i>
-                        </div>
-                    </li>
-                </ul>
-            </div>
+            {generateWeekStructure(dayNames)}
         </div>
     );
+}
+
+function generateWeekStructure(dayNames) {
+    return dayNames.map((dayName, index) => {
+        return (
+            <div className="day">
+                <div className="title">
+                    {dayName}
+                </div>
+                <ul className="todo-list">
+                    <li className="todo-list-item">
+                        <div className="description">
+                            Lorem ipsum dolor  amet
+                        </div>
+                        <div className="action-buttons">
+                            <i class="fas fa-trash-alt"></i>
+                        </div>
+                    </li>
+                    <li className="todo-list-item">
+                        <div className="description">
+                            Lorem ipsum  sit amet
+                        </div>
+                        <div className="action-buttons">
+                            <i class="fas fa-trash-alt delete"></i>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        );
+    });
 }
 
 export default Week;
