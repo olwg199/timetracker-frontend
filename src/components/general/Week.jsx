47110a7fcd266b3a88cd9f3c6ae41d778aebfc48
Oleg Yastrebov <olwg199@gmail.com>
2021-01-22 21:17:15 -0500

    Add styling to WeekView

--- src/components/general/Week.jsx
+++ src/components/general/Week.jsx
@@ -2,8 +2,174 @@ import React from "react";
 
 function Week(props) {
     return (
-        <div>
-            Week
+        <div className="days">
+            <div className="day">
+                <div className="title">
+                    Saturday
+                </div>
+                <ul className="todo-list">
+                    <li className="todo-list-item">
+                        <div className="description">
+                        Lorem ipsum dolor sit amet
+                        </div>
+                        <div className="action-buttons">
+                            <i class="fas fa-trash-alt"></i>
+                        </div>
+                    </li>
+                    <li className="todo-list-item">
+                    <div className="description">
+                        Lorem ipsum dolor sit amet
+                        </div>
+                        <div className="action-buttons">
+                            <i class="fas fa-trash-alt delete"></i>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="day">
+                <div className="title">
+                    Saturday
+                </div>
+                <ul className="todo-list">
+                    <li className="todo-list-item">
+                        <div className="description">
+                        Lorem ipsum dolor sit amet
+                        </div>
+                        <div className="action-buttons">
+                            <i class="fas fa-trash-alt"></i>
+                        </div>
+                    </li>
+                    <li className="todo-list-item">
+                    <div className="description">
+                        Lorem ipsum dolor sit amet
+                        </div>
+                        <div className="action-buttons">
+                            <i class="fas fa-trash-alt delete"></i>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="day">
+                <div className="title">
+                    Saturday
+                </div>
+                <ul className="todo-list">
+                    <li className="todo-list-item">
+                        <div className="description">
+                        Lorem ipsum dolor sit amet
+                        </div>
+                        <div className="action-buttons">
+                            <i class="fas fa-trash-alt"></i>
+                        </div>
+                    </li>
+                    <li className="todo-list-item">
+                    <div className="description">
+                        Lorem ipsum dolor sit amet
+                        </div>
+                        <div className="action-buttons">
+                            <i class="fas fa-trash-alt delete"></i>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="day">
+                <div className="title">
+                    Saturday
+                </div>
+                <ul className="todo-list">
+                    <li className="todo-list-item">
+                        <div className="description">
+                        Lorem ipsum dolor sit amet
+                        </div>
+                        <div className="action-buttons">
+                            <i class="fas fa-trash-alt"></i>
+                        </div>
+                    </li>
+                    <li className="todo-list-item">
+                    <div className="description">
+                        Lorem ipsum dolor sit amet
+                        </div>
+                        <div className="action-buttons">
+                            <i class="fas fa-trash-alt delete"></i>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="day">
+                <div className="title">
+                    Saturday
+                </div>
+                <ul className="todo-list">
+                    <li className="todo-list-item">
+                        <div className="description">
+                        Lorem ipsum dolor sit amet
+                        </div>
+                        <div className="action-buttons">
+                            <i class="fas fa-trash-alt"></i>
+                        </div>
+                    </li>
+                    <li className="todo-list-item">
+                    <div className="description">
+                        Lorem ipsum dolor sit amet
+                        </div>
+                        <div className="action-buttons">
+                            <i class="fas fa-trash-alt delete"></i>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="day">
+                <div className="title">
+                    Saturday
+                </div>
+                <ul className="todo-list">
+                    <li className="todo-list-item">
+                        <div className="description">
+                        Lorem ipsum dolor sit amet
+                        </div>
+                        <div className="action-buttons">
+                            <i class="fas fa-trash-alt"></i>
+                        </div>
+                    </li>
+                    <li className="todo-list-item">
+                    <div className="description">
+                        Lorem ipsum dolor sit Lorem ipsum dolor sit 
+                        </div>
+                        <div className="action-buttons">
+                            <i class="fas fa-trash-alt delete"></i>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="day">
+                <div className="title">
+                    Saturday
+                </div>
+                <ul className="todo-list">
+                    <li className="todo-list-item">
+                        <div className="description">
+                        Lorem ipsum dolor  amet
+                        </div>
+                        <div className="action-buttons">
+                            <i class="fas fa-trash-alt"></i>
+                        </div>
+                    </li>
+                    <li className="todo-list-item">
+                    <div className="description">
+                        Lorem ipsum  sit amet
+                        </div>
+                        <div className="action-buttons">
+                            <i class="fas fa-trash-alt delete"></i>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 }
