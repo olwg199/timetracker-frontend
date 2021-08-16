d940f57c06b8d495fec02fd00df89bc38920580e
olwg199 <olwg199@gmail.com>
2021-08-16 13:22:04 -0400

    Remove add goal functionality from global App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,25 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from "components/Header";
-import MainArea from "components/MainArea"
+import MainArea from "components/MainArea";
 import { applyMiddleware, createStore } from "redux";
 import Reducers from "reducers";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
-import Modal from "components/UI/Modal";
-import AddUpdateGoal from "components/UI/AddUpdateGoal";
-import AddButton from "components/UI/AddButton";
 import thunk from "redux-thunk";
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const store = createStore(
   Reducers,
-  composeWithDevTools(
-    applyMiddleware(thunk)
-  )
+  composeWithDevTools(applyMiddleware(thunk))
 );
 
 function App() {
-  const [addGoalViewActive, setAddGoalViewActive] = useState(false);
-
-
   return (
     <Provider store={store}>
       <Router>
         <div className="container">
           <Header />
           <MainArea />
-          <Modal active={addGoalViewActive} setActive={setAddGoalViewActive} title="Add goal">
-            <AddUpdateGoal setActive={setAddGoalViewActive} />
-          </Modal>
-          <AddButton active={addGoalViewActive} setActive={setAddGoalViewActive} />
         </div>
       </Router>
     </Provider>
--- src/components/MainArea/Goals.jsx
+++ src/components/MainArea/Goals.jsx
@@ -1,4 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import Modal from "components/UI/Modal";
+import AddUpdateGoal from "components/UI/AddUpdateGoal";
+import AddButton from "components/UI/AddButton";
 import GoalView from "components/UI/GoalView";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchGoals, deleteGoal } from "actions/goals-actions";
@@ -7,55 +10,81 @@ import Accordion from "components/UI/Accordion";
 import "css/MainArea/goals.css";
 
 function Goals(props) {
-    const dispatch = useDispatch();
-    const {isLoading, list} = useSelector(state => state.goals);
+  const dispatch = useDispatch();
+  const [addGoalViewActive, setAddGoalViewActive] = useState(false);
+  const { isLoading, list } = useSelector((state) => state.goals);
 
-    const goals = list.reduce((goals, goal) => {
-        if (goal.frequency === "daily") {
-            goals.daily.push(<GoalView goal={goal} key={goal.id} deleteGoal={() => { dispatch(deleteGoal(goal.id)) }} />);
-        }
-        if (goal.frequency === "monthly") {
-            goals.monthly.push(<GoalView goal={goal} key={goal.id} deleteGoal={() => { dispatch(deleteGoal(goal.id)) }} />);
-        }
-        if (goal.frequency === "annual") {
-            goals.annual.push(<GoalView goal={goal} key={goal.id} deleteGoal={() => { dispatch(deleteGoal(goal.id)) }} />);
-        }
-        return goals;
-    }, { daily: [], monthly: [], annual: [] });
+  const goals = list.reduce(
+    (goals, goal) => {
+      if (goal.frequency === "daily") {
+        goals.daily.push(
+          <GoalView
+            goal={goal}
+            key={goal.id}
+            deleteGoal={() => {
+              dispatch(deleteGoal(goal.id));
+            }}
+          />
+        );
+      }
+      if (goal.frequency === "monthly") {
+        goals.monthly.push(
+          <GoalView
+            goal={goal}
+            key={goal.id}
+            deleteGoal={() => {
+              dispatch(deleteGoal(goal.id));
+            }}
+          />
+        );
+      }
+      if (goal.frequency === "annual") {
+        goals.annual.push(
+          <GoalView
+            goal={goal}
+            key={goal.id}
+            deleteGoal={() => {
+              dispatch(deleteGoal(goal.id));
+            }}
+          />
+        );
+      }
+      return goals;
+    },
+    { daily: [], monthly: [], annual: [] }
+  );
 
-    useEffect(() => {
-        dispatch(fetchGoals());
-    }, [ list.length, dispatch]);
+  useEffect(() => {
+    dispatch(fetchGoals());
+  }, [list.length, dispatch]);
 
-    return (
-        <section className="goal-list">
+  return (
+    <section className="goal-list">
+      <Accordion title="Daily goals">
+        {goals.daily.length > 0
+          ? goals.daily
+          : isLoading
+          ? "Loading"
+          : "Sorry, no goals available"}
+      </Accordion>
+      <Accordion title="Monthly goals">
+        {goals.monthly.length > 0 ? goals.monthly : "Sorry, no goals available"}
+      </Accordion>
+      <Accordion title="Annual goals">
+        {goals.annual.length > 0 ? goals.annual : "Sorry, no goals available"}
+      </Accordion>
 
-{
+      <AddButton active={addGoalViewActive} setActive={setAddGoalViewActive} />
 
-}
-            <Accordion title="Daily goals">
-                {goals.daily.length > 0 ?
-                    goals.daily
-                    :
-                    isLoading ? "Loading": "Sorry, no goals available"
-                }
-            </Accordion>
-            <Accordion title="Monthly goals">
-                {goals.monthly.length > 0 ?
-                    goals.monthly
-                    :
-                    "Sorry, no goals available"
-                }
-            </Accordion>
-            <Accordion title="Annual goals">
-                {goals.annual.length > 0 ?
-                    goals.annual
-                    :
-                    "Sorry, no goals available"
-                }
-            </Accordion>
-        </section>
-    );
+      <Modal
+        active={addGoalViewActive}
+        setActive={setAddGoalViewActive}
+        title="Add goal"
+      >
+        <AddUpdateGoal setActive={setAddGoalViewActive} />
+      </Modal>
+    </section>
+  );
 }
 
 export default Goals;
