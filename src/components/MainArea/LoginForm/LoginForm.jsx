cc2f88a048cfb4f773fec7118435c4bfcd835533
olwg199 <olwg199@gmail.com>
2021-08-16 20:53:47 -0400

    Fix buttons on login/regiser forms

--- src/components/MainArea/LoginForm/LoginForm.jsx
+++ src/components/MainArea/LoginForm/LoginForm.jsx
@@ -1,34 +1,47 @@
-import React, { useState } from 'react';
-import Button from 'components/UI/Button';
+import React, { useState } from "react";
+import Button from "components/UI/Button";
 import InputGroup from "components/UI/InputGroup";
 import { useDispatch, useSelector } from "react-redux";
-import { login } from 'actions/user-actions';
-import { Redirect } from 'react-router-dom';
+import { login } from "actions/user-actions";
+import { Redirect } from "react-router-dom";
 
 import "css/MainArea/loginForm.css";
 
 export default function LoginForm() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const dispatch = useDispatch();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
 
-    const { isAuth } = useSelector(state => state.user);
+  const { isAuth } = useSelector((state) => state.user);
 
-    function loginUser() {
-        dispatch(login(username, password));
-        setPassword("");
-    }
+  function loginUser() {
+    dispatch(login(username, password));
+    setPassword("");
+  }
 
-    return (
-
-        isAuth ?
-            <Redirect to="/" />
-            :
-            <div id="login-form">
-                <InputGroup type="text" name="Username" setValue={(e) => { setUsername(e.target.value) }} value={username} />
-                <InputGroup type="password" name="Password" setValue={(e) => { setPassword(e.target.value) }} value={password} />
-                <Button text="Login" href="#" onClick={loginUser} classList={["btn", "btn-login"]} />
-            </div>
-
-    )
+  return isAuth ? (
+    <Redirect to="/" />
+  ) : (
+    <div id="login-form">
+      <InputGroup
+        type="text"
+        name="Username"
+        setValue={(e) => {
+          setUsername(e.target.value);
+        }}
+        value={username}
+      />
+      <InputGroup
+        type="password"
+        name="Password"
+        setValue={(e) => {
+          setPassword(e.target.value);
+        }}
+        value={password}
+      />
+      <Button href="#" onClick={loginUser} classList={["btn", "btn-login"]}>
+        Login
+      </Button>
+    </div>
+  );
 }
--- src/components/MainArea/RegisterForm/RegisterForm.jsx
+++ src/components/MainArea/RegisterForm/RegisterForm.jsx
@@ -1,36 +1,60 @@
-import React, { useState } from 'react';
-import Button from 'components/UI/Button';
+import React, { useState } from "react";
+import Button from "components/UI/Button";
 import InputGroup from "components/UI/InputGroup";
 import { useDispatch, useSelector } from "react-redux";
-import { register } from 'actions/user-actions';
-import { Redirect } from 'react-router-dom';
+import { register } from "actions/user-actions";
+import { Redirect } from "react-router-dom";
 
 import "css/MainArea/registerForm.css";
 
 export default function LoginForm() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [email, setEmail] = useState("");
-    const dispatch = useDispatch();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const dispatch = useDispatch();
 
-    const { isAuth } = useSelector(state => state.user);
+  const { isAuth } = useSelector((state) => state.user);
 
-    function registerUser() {
-        dispatch(register(username, password, email));
-        setPassword("");
-    }
+  function registerUser() {
+    dispatch(register(username, password, email));
+    setPassword("");
+  }
 
-    return (
-
-        isAuth ?
-            <Redirect to="/" />
-            :
-            <div id="register-form">
-                <InputGroup type="text" name="Username" setValue={(e) => { setUsername(e.target.value) }} value={username} />
-                <InputGroup type="password" name="Password" setValue={(e) => { setPassword(e.target.value) }} value={password} />
-                <InputGroup type="email" name="Email" setValue={(e) => { setEmail(e.target.value) }} value={email} />
-                <Button text="Register" href="#" onClick={registerUser} classList={["btn", "btn-register"]} />
-            </div>
-
-    )
+  return isAuth ? (
+    <Redirect to="/" />
+  ) : (
+    <div id="register-form">
+      <InputGroup
+        type="text"
+        name="Username"
+        setValue={(e) => {
+          setUsername(e.target.value);
+        }}
+        value={username}
+      />
+      <InputGroup
+        type="password"
+        name="Password"
+        setValue={(e) => {
+          setPassword(e.target.value);
+        }}
+        value={password}
+      />
+      <InputGroup
+        type="email"
+        name="Email"
+        setValue={(e) => {
+          setEmail(e.target.value);
+        }}
+        value={email}
+      />
+      <Button
+        href="#"
+        onClick={registerUser}
+        classList={["btn", "btn-register"]}
+      >
+        Register
+      </Button>
+    </div>
+  );
 }
